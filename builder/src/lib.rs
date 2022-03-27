c171a334d1e19d0e54c259b9da50eec85d2b9aa3
guricerin <chanbo1e9@gmail.com>
2022-03-27 20:44:29 +0900

    builder 01

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,17 @@
 use proc_macro::TokenStream;
+use syn::{parse_macro_input, DeriveInput};
+use quote::quote;
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    // Parse the input tokens into a syntax tree
+    let _input = parse_macro_input!(input as DeriveInput);
 
-    unimplemented!()
+    // Build the output, possibly using quasi-quotation
+    let expanded = quote!{
+
+    };
+
+    // Hand the output tokens back to the compiler
+    TokenStream::from(expanded)
 }
